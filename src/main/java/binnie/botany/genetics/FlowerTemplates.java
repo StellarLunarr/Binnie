5988871448f465f2e5575fedcb672009fe8bed1c
boubou19 <miisterunknown@gmail.com>
2026-08-26 01:27:44 +0200

    Fix allocation hell hole (#119)

--- src/main/java/binnie/botany/genetics/FlowerTemplates.java
+++ src/main/java/binnie/botany/genetics/FlowerTemplates.java
@@ -8,7 +8,13 @@ import forestry.core.genetics.alleles.EnumAllele;
 
 public class FlowerTemplates {
 
+    private static IAllele[] defaultTemplate;
+
     public static IAllele[] getDefaultTemplate() {
+        if (defaultTemplate != null) {
+            return defaultTemplate.clone();
+        }
+
         IAllele[] alleles = new IAllele[EnumFlowerChromosome.VALUES.length];
         alleles[EnumFlowerChromosome.SPECIES.ordinal()] = FlowerSpecies.POPPY;
         alleles[EnumFlowerChromosome.PRIMARY.ordinal()] = AlleleHelper.getAllele(EnumFlowerColor.RED);
@@ -22,6 +28,13 @@ public class FlowerTemplates {
         alleles[EnumFlowerChromosome.PH_TOLERANCE.ordinal()] = AlleleHelper.getAllele(EnumTolerance.NONE);
         alleles[EnumFlowerChromosome.SAPPINESS.ordinal()] = AlleleHelper.getAllele(EnumAllele.Sappiness.AVERAGE);
         alleles[EnumFlowerChromosome.STEM.ordinal()] = AlleleHelper.getAllele(EnumFlowerColor.GREEN);
-        return alleles;
+
+        for (IAllele allele : alleles) {
+            // Do not cache a partial template during registry initialization.
+            if (allele == null) return alleles;
+        }
+
+        defaultTemplate = alleles;
+        return defaultTemplate.clone();
     }
 }
